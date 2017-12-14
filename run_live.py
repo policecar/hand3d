e1e2d59dfacbc624430495f0ea06f03b2cd353fb
priska <priska@23bit.net>
2017-12-14 16:48:39 +0100

    Remove remnants from run.py

--- run_live.py
+++ run_live.py
@@ -29,8 +29,6 @@ from utils.general import detect_keypoints, trafo_coords, plot_hand, plot_hand_3
 
 if __name__ == '__main__':
 
-    # image_list = list(range(10))
-
     # instantiate camera objects
     serv = pyrs.Service()
     cam = serv.Device(device_id = 0, streams = [pyrs.stream.ColorStream(fps = 30)])
@@ -82,12 +80,6 @@ if __name__ == '__main__':
         keypoints_scoremap_v = np.squeeze(keypoints_scoremap_v)
         keypoint_coord3d_v = np.squeeze(keypoint_coord3d_v)
 
-        # try:
-        #     # del ax.collections[-1]
-        #     # plt.clf()
-        # except:
-        #     pass
-
         # post processing
         image_crop_v = ((image_crop_v + 0.5) * 255).astype('uint8')
         coord_hw_crop = detect_keypoints(np.squeeze(keypoints_scoremap_v))
@@ -104,21 +96,3 @@ if __name__ == '__main__':
         # plt.title(title)
         plt.draw()
         plt.pause(0.001)
-
-        # # visualize
-        # fig = plt.figure(1)
-        # ax1 = fig.add_subplot(221)
-        # ax2 = fig.add_subplot(222)
-        # ax3 = fig.add_subplot(223)
-        # ax4 = fig.add_subplot(224, projection='3d')
-        # ax1.imshow(image_raw)
-        # plot_hand(coord_hw, ax1)
-        # ax2.imshow(image_crop_v)
-        # plot_hand(coord_hw_crop, ax2)
-        # ax3.imshow(np.argmax(hand_scoremap_v, 2))
-        # plot_hand_3d(keypoint_coord3d_v, ax4)
-        # ax4.view_init(azim=-90.0, elev=-90.0)  # aligns the 3d coord with the camera view
-        # ax4.set_xlim([-3, 3])
-        # ax4.set_ylim([-3, 1])
-        # ax4.set_zlim([-3, 3])
-        # plt.show()
